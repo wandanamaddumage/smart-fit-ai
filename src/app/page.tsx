de557be142cba4d06e1d18bbe6c517956d6d4280
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-05-09 11:05:03 +0530

    feat: Setting Up Authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+"use client";
+import { SignedIn, SignOutButton } from "@clerk/clerk-react";
+import { SignedOut, SignInButton } from "@clerk/nextjs";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default HomePage;
