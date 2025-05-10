f99d698a0bd02b2ee976bb7c29af3267ecd0d4cc
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-05-10 23:20:31 +0530

    feat: Setting Up Our Database

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ConvexClerkProvider>
       <html
         lang="en"
         data-darkreader-proxy-injected="true"
@@ -37,6 +37,6 @@ export default function RootLayout({
           {children}
         </body>
       </html>
-    </ClerkProvider>
+    </Convex>
   );
 }
