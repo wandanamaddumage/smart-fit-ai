ccfc17c61062400981167f1384460cbe3b41440d
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-05-10 23:27:36 +0530

    feat: Setting Up Our Database

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: process.env.VITE_CLERK_FRONTEND_API_URL,
+      domain: "https://peaceful-baboon-38.clerk.accounts.dev/",
       applicationID: "convex",
     },
   ],
