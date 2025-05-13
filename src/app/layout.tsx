e332455f2ecd30c3b3eb620b74ffc2ed5dc49685
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-05-14 01:47:03 +0530

    fix: errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,16 +36,18 @@ export default function RootLayout({
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Navbar />
+          <div className="px-2">
+            <Navbar />
 
-          {/* GRID BACKGROUND */}
-          <div className="fixed inset-0 -z-1">
-            <div className="absolute inset-0 bg-gradient-to-b from-background via-background to-background"></div>
-            <div className="absolute inset-0 bg-[linear-gradient(var(--cyber-grid-color)_1px,transparent_1px),linear-gradient(90deg,var(--cyber-grid-color)_1px,transparent_1px)] bg-[size:20px_20px]"></div>
+            {/* GRID BACKGROUND */}
+            <div className="fixed inset-0 -z-1">
+              <div className="absolute inset-0 bg-gradient-to-b from-background via-background to-background"></div>
+              <div className="absolute inset-0 bg-[linear-gradient(var(--cyber-grid-color)_1px,transparent_1px),linear-gradient(90deg,var(--cyber-grid-color)_1px,transparent_1px)] bg-[size:20px_20px]"></div>
+            </div>
+
+            <main className="pt-24 flex-grow">{children}</main>
+            <Footer />
           </div>
-
-          <main className="pt-24 flex-grow">{children}</main>
-          <Footer />
         </body>
       </html>
     </ConvexClerkProvider>
