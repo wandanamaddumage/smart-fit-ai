b8fec93507f13d2c2c7d61213248d9266b54914e
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-05-13 12:59:00 +0530

    feat: add Generate Program Page UI

--- src/lib/vapi.ts
+++ src/lib/vapi.ts
@@ -1,3 +1,6 @@
 import Vapi from "@vapi-ai/web";
 
-export const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_API_KEY!);
+const apiKey = process.env.NEXT_PUBLIC_VAPI_API_KEY;
+if (!apiKey) throw new Error("VAPI API Key is missing");
+
+export const vapi = new Vapi(apiKey);
